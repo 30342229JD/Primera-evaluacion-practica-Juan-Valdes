4fe3bccb739c8eaa3db5be8211e8ab148d321887
30342229JD <78279074+30342229JD@users.noreply.github.com>
2021-02-04 22:56:34 -0400

    Add files via upload

---
+++ ejercicio.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Examen_1
+{
+    class ejercicio
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+
+            a = 10;
+            b = 10;
+            c = 350;
+
+
+            //bcbc
+
+            Console.Write("Tu saldo anterior era de: ");
+            Console.Write(b);
+            Console.Write(c);
+            Console.Write(b);
+            Console.WriteLine(c);
+
+            Console.WriteLine("-----");
+
+            //aacb
+            a = 5;
+            b = 10;
+            c = 8;
+
+            Console.Write("Tienes actualmente un saldo de: ");
+            Console.Write(a);
+            Console.Write(a);
+            Console.Write(c);
+            Console.WriteLine(b);
+
+            Console.WriteLine("-----");
+            //bca
+            a = 78;
+            b = 100;
+            c = 8;
+
+            Console.Write("Tu penúltima compra fue de : ");
+            Console.Write(b);
+            Console.Write(c);
+            Console.WriteLine(a);
+
+            Console.WriteLine("----Su sesión ha finalizado----");
+            
+
+        }
+    }
+}
